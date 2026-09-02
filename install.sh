0c76f6f35e08358c7f06a00da19f285b5ccc80c0
Carlton-Perkins <carlton.perkins@matrixspace.com>
2026-09-02 15:29:43 -0400

    replace with nix based install always, dont expect system to have nix or gh

--- install.sh
+++ install.sh
@@ -1,43 +1,24 @@
 #!/bin/sh
 set -ex
 
-GITHUB_USERNAME="Carlton-Perkins"
+echo "[INSTALL] Nix (single-user)"
+curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install | sh -s -- --no-daemon
 
-# If nix is installed use nixdotfiles instead
-echo "[CHECK] Is Nix installed?"
-if command -v nix &>/dev/null; then
-  echo "[SETUP] Nix dotfiles"
-  gh repo clone Carlton-Perkins/nix-dotfiles ~/nix-dotfiles
-  cd ~/nix-dotfiles
+# Make Nix and packages installed into the user profile available in this shell.
+. "$HOME/.nix-profile/etc/profile.d/nix.sh"
 
-  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-  nix-channel --update
+echo "[SETUP] Nix channels"
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
 
-  nix-shell '<home-manager>' -A install
-  home-manager switch --flake ./ -b backup
+echo "[INSTALL] GitHub CLI"
+nix-env -iA nixpkgs.gh
 
-  echo "[INSTALL] Nix dotfiles complete"
-  exit 0
-fi
+echo "[SETUP] Nix dotfiles"
+gh repo clone Carlton-Perkins/nix-dotfiles "$HOME/nix-dotfiles"
+cd "$HOME/nix-dotfiles"
 
-echo "[INSTALL] Ohmyzsh"
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+nix-shell '<home-manager>' -A install
+home-manager switch --flake ./ -b backup
 
-echo "[INSTALL] Powerlevel10k"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-echo "[INSTALL] cargo-binstall"
-curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-
-echo "[INSTALL] cargo deps"
-cargo binstall -y bat du-dust bandwhich fd-find ripgrep exa git-delta
-
-echo "[INSTALL] Chezmoi"
-export BINDIR=$HOME/.local/bin
-sh -c "$(curl -fsLS chezmoi.io/get)"
-
-echo "[SETUP] Chezmoi"
-$BINDIR/chezmoi init $GITHUB_USERNAME
-
-echo "[APPLY] apply dotfiles"
-$BINDIR/chezmoi apply
+echo "[INSTALL] Nix dotfiles complete"
